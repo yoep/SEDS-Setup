7262a5316821c30683420d13cc591bbfd44bf03a
Arghlex M <ArghArgh200@users.noreply.github.com>
2015-02-21 12:05:05 -0800

    Update start.sh to add a bit more functions
    Silenced all the WINEDEBUG noise
    Made sure people could wipe every folder except for their config and world, and still have it setup properly.
    Later: adding a link to Andy_S's world cleaner for dead NPC entries.

--- start.sh
+++ start.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#put this script in ~/spaceengineers/start.sh
+#run 'chmod +x ~/spaceengineers/start.sh'
+#and '~/spaceengineers/start.sh setup'
 service=spaceengineers
 procname=SpaceEngineersDedicated.exe
 cd $HOME/spaceengineers
@@ -8,7 +11,7 @@ case "$1" in
         start)
                 #login to steam and fetch the latest gamefiles
                 cd $HOME/spaceengineers
-                steamuser=`cat .steamuser`
+                steamuser=`cat $HOME/spaceengineers/Steamcmd/.steamuser`
                 cd Steamcmd
                 WINEDEBUG=-all wine steamcmd.exe +force_install_dir C:\\users\\$whoami\\Desktop\\spaceengineers\\client +login $steamuser +app_update 244850 -verify +quit
                 cd ..
@@ -16,7 +19,10 @@ case "$1" in
                 #clear old binaries and get new ones
                 rm -rf DedicatedServer DedicatedServer64 Content
                 unzip client/Tools/DedicatedServer.zip
-                cd config
+                cd config/Saves/SEDSWorld
+                echo "Cleaning world of dead NPC entries - Credits to Andy_S of #space-engineers"
+                wget -q -O ../../worldcleaner.py <LINK SOON>
+                python ../../worldcleaner.py
 
                 #start the DS
                 cd $HOME/.wine/drive_c/users/$whoami/Desktop/spaceengineers/DedicatedServer
@@ -26,54 +32,58 @@ case "$1" in
                 #copy server world and log to backups and logs directories
                 cd ../config
                 mv SpaceEngineersDedicated.log logs/server-$logstamper.log
-                cp -rf Saves/SBGSEWorld backups/world-$logstamper-svhalt
+                cp -rf Saves/SEDSWorld backups/world-$logstamper-svhalt
         ;;
         setup)  #run only once.
-                echo "Press enter to confirm complete wipe of your WINE's configuration directory. If you have installed anything under regular WINE and want to keep it, do not press enter!"
+                echo "Press enter to confirm complete wipe of your WINE's configuration directory. If you have installed anything under regular WINE and want to keep it, press Ctrl-C now!"
                 read things
                 echo "ARE YOU SURE?"
                 read things
                 echo "ARE YOU REALLY SURE?"
                 read things
+                echo "Wiping WINE installation."
                 rm -rf $HOME/.wine
 
                 #grab steamcmd, make some directories.
-                mkdir $home/spaceengineers/config
-                mkdir $home/spaceengineers/config/backups
-                mkdir $home/spaceengineers/config/logs
+                mkdir $HOME/spaceengineers/config
+                mkdir $HOME/spaceengineers/client
+                mkdir $HOME/spaceengineers/config/backups
+                mkdir $HOME/spaceengineers/config/logs
+                rm -rf $HOME/spaceengineers/Steamcmd
                 mkdir -p $HOME/spaceengineers/Steamcmd
                 cd $HOME/spaceengineers/Steamcmd
-                wget -O steamcmd.zip http://media.steampowered.com/installer/steamcmd.zip
-                unzip steamcmd.zip
+                echo "Downloading SteamCMD"
+                wget -q -O steamcmd.zip http://media.steampowered.com/installer/steamcmd.zip
+                unzip steamcmd.zip >/dev/null
 
                 #configure our wine directory and make some symlinks
                 cd $HOME
-                echo "configuring WINE and installing dependencies."
-                WINEARCH=win32 winecfg
-                winetricks -q msxml3
-                winetricks -q dotnet40
-                wget -O $HOME/.wine/drive_c/windows/system32/oleaut32.dll http://pitchblack.arghlex.net/oleaut32.dll
-                ln -s $HOME/.wine/drive_c/users/$whoami/Desktop/spaceengineers $HOME/spaceengineers/
-                ln -s $HOME/.wine/drive_c/users/$whoami/Application\ Data/SpaceEngineersDedicated $HOME/spaceengineers/config/
+                echo "Configuring WINE and installing dependencies."
+                WINEDEBUG=-all WINEARCH=win32 winecfg > /dev/null
+                WINEDEBUG=-all winetricks -q msxml3 > /dev/null
+                WINEDEBUG=-all winetricks -q dotnet40 > /dev/null
+                wget -q -O $HOME/.wine/drive_c/windows/system32/oleaut32.dll http://pitchblack.arghlex.net/oleaut32.dll
+                ln -s $HOME/spaceengineers $HOME/.wine/drive_c/users/$whoami/Desktop/spaceengineers
+                ln -s $HOME/spaceengineers/config $HOME/.wine/drive_c/users/$whoami/Application\ Data/SpaceEngineersDedicated
+                echo "Initial setup complete."
 
                 #login to steam for the first time, and allow steamcmd to run itself
                 echo "We'll now try and run steamcmd. In order to install the DS, you need to have a steam account with the game purchased and activated."
                 echo "Steam username:"
                 read steamuser
-                echo $steamuser > $HOME/spaceengineers/.steamuser
+                echo $steamuser > $HOME/spaceengineers/Steamcmd/.steamuser
                 echo "Steam password (Not stored, if you have steamguard enabled like a smart person, go get the code when it asks for one.):"
                 read steampass
                 #run twice because the first time we need to make steamcmd download its files before attempting a login
+                cd $HOME/spaceengineers/Steamcmd/
                 WINEDEBUG=-all wine steamcmd.exe +exit
                 WINEDEBUG=-all wine steamcmd.exe +login $steamuser $steampass +exit
-                echo "If you did not see something download above this line, something went wrong. Get on the forums and ask around."
-                echo "Alright, now that you have the dedicated server installed, go make a config with the copy your game files have locally. You'll need to edit it and change the <LoadWorld /> part to look (roughly) like this: <LoadWorld>C:\users\<your username>\Application Data\SpaceEngineersDedicated\Saves\YourWorldName</LoadWorld>."
-                echo "Donate to keep things like this going! http://arghlex.net/?page=donators !"
+                echo "Setup complete. Please place your server's .cfg file in ~/spaceengineers/config/SpaceEngineers-Dedicated.cfg.  You'll need to edit it and change the <LoadWorld /> part to read: <LoadWorld>C:\users\$whoami\Application Data\SpaceEngineersDedicated\Saves\SEDSWorld</LoadWorld>."
         ;;
         backupworld) #put an entry in your crontab pointing to this script with the first argument being 'backupworld'.
                 logstampworld=`date +%s`
                 cd $HOME/spaceengineers/config
-                cp -rf Saves/SBGSEWorld backups/world-$logstampworld
+                cp -rf Saves/SEDSWorld backups/world-$logstampworld
         ;;
         *)
                 if ps ax | grep -v grep | grep $procname > /dev/null
